351191b47f573fa86283b2552b2222841b2faac5
MarcosPalladino <marcospalladino@gmail.com>
2020-02-27 22:04:32 -0300

    promisses e async await

---
+++ ler_arquivo.js
@@ -0,0 +1,73 @@
+var fs = require('fs');
+
+const lerArquivo = function(arquivo) {
+	return new Promise(function(resolve, reject){
+		fs.readFile(arquivo,'utf8', function(err,contents){
+			if(err){
+				reject(err);
+			}
+			else{
+				resolve(contents);
+			}
+		})
+	})
+}
+
+
+
+
+let c1 = "";
+let c2 = "";
+let c3 = "";
+let c4 = "";
+
+const init = async() => {
+	const contents1 = await lerArquivo("teste.txt");
+	const contents2 = await lerArquivo("teste2.txt");
+	const contents3 = await lerArquivo("teste3.txt");
+	const contents4 = await lerArquivo("teste4.txt");
+
+	console.log(contents1);
+	console.log(contents2);
+	console.log(contents3);
+	console.log(contents4);
+}
+
+init();
+
+
+//lerArquivo('teste.txt').then(function(contents) {
+//	console.log(contents)
+//	return lerArquivo('teste2.txt').then(function(contents){
+//		console.log(contents);
+//			return lerArquivo('teste3.txt').then(function(contents){
+//				console.log(contents);	
+//				return lerArquivo('teste4.txt').then(function(contents){
+//					console.log(contents);	
+//				})
+//			})
+//		})
+//	})
+
+//fs.readFile('teste.txt', 'utf8', function(err,contents){
+//	c1 = contents;
+//	console .log(contents);
+//	fs.readFile('teste2.txt', 'utf8', function(err,contents){
+//		c2 = contents;
+//		console .log(contents);
+//		fs.readFile('teste3.txt', 'utf8', function(err,contents){
+//			c3 = contents;
+//			console .log(contents);
+//			fs.readFile('teste4.txt', 'utf8', function(err,contents){
+//				c4 = contents;
+//				console .log(contents);
+//				console.log(`${c1} - ${c2} - ${c3} - ${c4}`)
+//			});
+//		});
+//	});
+//});
+
+
+
+
+
